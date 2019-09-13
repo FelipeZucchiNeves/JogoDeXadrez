967e79ff255afbde8ac397b6ab68b59d67928c56
Felipe Neves <felipe_neves_2@hotmail.com>
2019-09-13 12:15:27 -0300

    Começo da implementação do tabuleiro de xadrez

---
+++ Chess-Game/src/boardgame/Piece.java
@@ -0,0 +1,25 @@
+package boardgame;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	
+	
+	public Piece( Board board) {
+		this.board = board;
+		this.position = null;
+	}
+
+	//ONLY LAYER OF BOARD CAN SEE THIS METHOD
+	//SOMENTE A CAMADA DO TABULEITO PODE ENXERGAR ESTE MÉTODO
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+
+	
+
+}
