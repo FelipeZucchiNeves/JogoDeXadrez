7bb1293b7fd8206270acd3d192d84746ee8c57de
Felipe Neves <felipe_neves_2@hotmail.com>
2019-09-24 23:27:57 -0300

    Tratamento de exceção e limpar tela

--- Chess-Game/src/application/Program.java
+++ Chess-Game/src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -18,16 +20,23 @@ public class Program {
 		
 		
 		while (true) {
-			UI.printBoard(cm.getPieces());
-			System.out.println();
-			System.out.println("Source: ");
-			ChessPosition source = UI.readChessPosition(scn);
-			System.out.println();
-			System.out.println("Target :");
-			ChessPosition target = UI.readChessPosition(scn);
-			
-			ChessPiece capturedPiece = cm.performChessMove(source, target);
-			
+			try {
+				UI.clearSreen();
+				UI.printBoard(cm.getPieces());
+				System.out.println();
+				System.out.println("Source: ");
+				ChessPosition source = UI.readChessPosition(scn);
+				System.out.println();
+				System.out.println("Target :");
+				ChessPosition target = UI.readChessPosition(scn);
+				ChessPiece capturedPiece = cm.performChessMove(source, target);
+			}catch(ChessException e ) {
+				System.out.println(e.getMessage());
+				scn.nextLine();
+			}catch(InputMismatchException e) {
+				System.out.println(e.getMessage());
+				scn.nextLine();
+			}
 		}
 		
 	}
