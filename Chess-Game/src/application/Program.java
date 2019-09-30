528a7bc6af02869a48d7083cd3124616b51a5d17
Felipe Neves <felipe_neves_2@hotmail.com>
2019-09-30 06:05:43 -0300

    Implementando peças capturadas

--- Chess-Game/src/application/Program.java
+++ Chess-Game/src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -11,9 +13,11 @@ import chess.ChessPosition;
 public class Program {
 	
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
 		
+		
+		Scanner scn = new Scanner(System.in);
 		ChessMatch cm = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<ChessPiece>();
 		
 		// O console do eclipse n�o reconhece as cores rode direto pelo terminal caso ele tenha reconhecimento de cores.
 		
@@ -22,7 +26,7 @@ public class Program {
 		while (true) {
 			try {
 				UI.clearSreen();
-				UI.printMatch(cm);
+				UI.printMatch(cm, captured);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(scn);
@@ -36,6 +40,12 @@ public class Program {
 				System.out.println("Target :");
 				ChessPosition target = UI.readChessPosition(scn);
 				ChessPiece capturedPiece = cm.performChessMove(source, target);
+				
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
+				
 			}catch(ChessException e ) {
 				System.out.println(e.getMessage());
 				scn.nextLine();
--- Chess-Game/src/chess/ChessMatch.java
+++ Chess-Game/src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -11,12 +14,15 @@ public class ChessMatch {
 	private Board board;
 	private Color currentPlayer;
 	private int turn;
-	
+	private List <Piece> piecesOnTheBoard;
+	private List <Piece> capturedPieces;
 	
 	public ChessMatch() {
 		board = new Board(8,8);
 		turn = 1;
 		currentPlayer = Color.WHITE;
+		piecesOnTheBoard = new ArrayList<Piece>();
+		capturedPieces = new ArrayList<Piece>();
 		initialSetup();
 	}
 	
@@ -41,6 +47,7 @@ public class ChessMatch {
 	
 	private void placeNewPiece(char  column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		piecesOnTheBoard.add(piece);
 	}
 	
 	
@@ -71,6 +78,10 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		if(capturedPiece != null) {
+			piecesOnTheBoard.remove(capturedPiece);
+			capturedPieces.add(capturedPiece);
+		}
 		return capturedPiece;
 	}
 
