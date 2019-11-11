e0a16a839f7f757b6284c9f35169d6499eae5e9e
Felipe Neves <felipe_neves_2@hotmail.com>
2019-11-11 11:18:28 -0300

    Implementação do Peão

--- Chess-Game/src/chess/pieces/Pawn.java
+++ Chess-Game/src/chess/pieces/Pawn.java
@@ -18,8 +18,53 @@ public class Pawn extends ChessPiece{
 		boolean matriz [][] = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0, 0);
 		
+		if(getColor() == Color.WHITE) {
+			p.setValues(position.getRow()-1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			p.setValues(position.getRow()-2, position.getColumn());
+			Position p2 = new Position(position.getRow()-1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsPiece(p2) && getMoveCount()==0) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			p.setValues(position.getRow()-1, position.getColumn()-1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			p.setValues(position.getRow()-1, position.getColumn()+1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+		}else {
+			
+			p.setValues(position.getRow()+1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsPiece(p)) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			p.setValues(position.getRow()+2, position.getColumn());
+			Position p2 = new Position(position.getRow()-1, position.getColumn());
+			if(getBoard().positionExists(p) && !getBoard().thereIsPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsPiece(p2) && getMoveCount()==0) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			p.setValues(position.getRow()+1, position.getColumn()-1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			p.setValues(position.getRow()+1, position.getColumn()+1);
+			if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+				matriz [p.getRow()][p.getColumn()]=true;
+			}
+			
+		}
 		
-		return null;
+		
+		return matriz;
+	}
+	
+	@Override
+	public String toString() {
+		return "P";
 	}
 
 }
